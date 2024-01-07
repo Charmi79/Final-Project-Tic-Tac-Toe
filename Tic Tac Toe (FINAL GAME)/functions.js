333fedc4eb0a64ec38c26824676d15909dce094c
Charmi79 <145047396+Charmi79@users.noreply.github.com>
2024-01-07 14:30:41 -0700

    Update functions.js

--- Tic Tac Toe (FINAL GAME)/functions.js
+++ Tic Tac Toe (FINAL GAME)/functions.js
@@ -2,186 +2,305 @@
 
 // Draw Start Screen
 function drawStart() {
-  drawMainComponents();
+	drawMainComponents();
 
-  // Start Text
-  ctx.font = "60px Consolas";
-  ctx.fillStyle = "lightblue";
-  ctx.fillText("TIC TAC TOE", 240, 350);
+	// Start Text
+	ctx.font = "60px Consolas";
+	ctx.fillStyle = "lightblue";
+	ctx.fillText("TIC TAC TOE", 240, 350);
 
-  ctx.font = "30px Consolas";
-  ctx.fillText("CLICK INSIDE THE GRID BOXES TO PLACE YOUR X", 40, 450);
+	ctx.font = "30px Consolas";
+	ctx.fillText("CLICK INSIDE THE GRID BOXES TO PLACE YOUR PLAYER", 5, 450);
 }
 
 // Draw Game Elements
 function runGame() {
-  // LOGIC
-  findGridLoc();
-  outputImg();
+	// LOGIC
+	findGridLoc();
+	chgPlayerAndContent();
+	gameEndStatus();
 
-  // DRAW GAME
-  drawGame();
+	// DRAW GAME
+	drawGame();
 }
 
 function findGridLoc() {
-  if (mouseX < 266 && mouseY < 200) {
-    cell = 1;
-  } else if (mouseX < 533 && mouseX > 316 && mouseY < 200) {
-    cell = 2;
-  } else if (mouseX > 583 && mouseX < cnv.width && mouseY < 200) {
-    cell = 3;
-  } else if (mouseX < 266 && mouseY < 500) {
-    cell = 4;
-  } else if (mouseX < 533 && mouseX > 316 && mouseY < 500) {
-    cell = 5;
-  } else if (mouseX > 583 && mouseX < cnv.width && mouseY < 500) {
-    cell = 6;
-  } else if (mouseX < 266 && mouseY < cnv.height) {
-    cell = 7;
-  } else if (mouseX < 533 && mouseX > 316 && mouseY < cnv.height) {
-    cell = 8;
-  } else if (mouseX > 583 && mouseX < cnv.width && mouseY < cnv.height) {
-    cell = 9;
-  }
+	// Determine what cell the mouse is hovering over
+	if (mouseX < 266 && mouseY < 200) {
+		cell = 1;
+	} else if (mouseX < 533 && mouseX > 316 && mouseY < 200) {
+		cell = 2;
+	} else if (mouseX > 583 && mouseX < cnv.width && mouseY < 200) {
+		cell = 3;
+	} else if (mouseX < 266 && mouseY < 500) {
+		cell = 4;
+	} else if (mouseX < 533 && mouseX > 316 && mouseY < 500) {
+		cell = 5;
+	} else if (mouseX > 583 && mouseX < cnv.width && mouseY < 500) {
+		cell = 6;
+	} else if (mouseX < 266 && mouseY < cnv.height) {
+		cell = 7;
+	} else if (mouseX < 533 && mouseX > 316 && mouseY < cnv.height) {
+		cell = 8;
+	} else if (mouseX > 583 && mouseX < cnv.width && mouseY < cnv.height) {
+		cell = 9;
+	}
 }
 
-function outputImg() {
-  if (mouseIsPressed && currentPlayer === "X") {
-    if (cell === 1) {
-      cell1Content = "X";
-    } else if (cell === 2) {
-      cell2Content = "X";
-    } else if (cell === 3) {
-      cell3Content = "X";
-    } else if (cell === 4) {
-      cell4Content = "X";
-    } else if (cell === 5) {
-      cell5Content = "X";
-    } else if (cell === 6) {
-      cell6Content = "X";
-    } else if (cell === 7) {
-      cell7Content = "X";
-    } else if (cell === 8) {
-      cell8Content = "X";
-      v;
-    } else if (cell === 9) {
-      cell9Content = "X";
-    }
-  } else if (mouseIsPressed && currentPlayer === "O") {
-    if (cell === 1) {
-      cell1Content = "O";
-    } else if (cell === 2) {
-      cell2Content = "O";
-    } else if (cell === 3) {
-      cell3Content = "O";
-    } else if (cell === 4) {
-      cell4Content = "O";
-    } else if (cell === 5) {
-      cell5Content = "O";
-    } else if (cell === 6) {
-      cell6Content = "O";
-    } else if (cell === 7) {
-      cell7Content = "O";
-    } else if (cell === 8) {
-      cell8Content = "O";
-    } else if (cell === 9) {
-      cell9Content = "O";
-    }
-  }
+function chgPlayerAndContent() {
+	// Change the cell content and current player
+	if (mouseIsPressed && currentPlayer === "X") {
+		if (cell === 1 && cell1Content === "") {
+			cell1Content = "X";
+			currentPlayer = "O";
+		} else if (cell === 2 && cell2Content === "") {
+			cell2Content = "X";
+			currentPlayer = "O";
+		} else if (cell === 3 && cell3Content === "") {
+			cell3Content = "X";
+			currentPlayer = "O";
+		} else if (cell === 4 && cell4Content === "") {
+			cell4Content = "X";
+			currentPlayer = "O";
+		} else if (cell === 5 && cell5Content === "") {
+			cell5Content = "X";
+			currentPlayer = "O";
+		} else if (cell === 6 && cell6Content === "") {
+			cell6Content = "X";
+			currentPlayer = "O";
+		} else if (cell === 7 && cell7Content === "") {
+			cell7Content = "X";
+			currentPlayer = "O";
+		} else if (cell === 8 && cell8Content === "") {
+			cell8Content = "X";
+			currentPlayer = "O";
+		} else if (cell === 9 && cell9Content === "") {
+			cell9Content = "X";
+			currentPlayer = "O";
+		}
+	} else if (mouseIsPressed && currentPlayer === "O") {
+		if (cell === 1 && cell1Content === "") {
+			cell1Content = "O";
+			currentPlayer = "X";
+		} else if (cell === 2 && cell2Content === "") {
+			cell2Content = "O";
+			currentPlayer = "X";
+		} else if (cell === 3 && cell3Content === "") {
+			cell3Content = "O";
+			currentPlayer = "X";
+		} else if (cell === 4 && cell4Content === "") {
+			cell4Content = "O";
+			currentPlayer = "X";
+		} else if (cell === 5 && cell5Content === "") {
+			cell5Content = "O";
+			currentPlayer = "X";
+		} else if (cell === 6 && cell6Content === "") {
+			cell6Content = "O";
+			currentPlayer = "X";
+		} else if (cell === 7 && cell7Content === "") {
+			cell7Content = "O";
+			currentPlayer = "X";
+		} else if (cell === 8 && cell8Content === "") {
+			cell8Content = "O";
+			currentPlayer = "X";
+		} else if (cell === 9 && cell9Content === "") {
+			cell9Content = "O";
+			currentPlayer = "X";
+		}
+	}
+}
+
+function gameEndStatus() {
+	// Check for a win
+	if (
+		// Rows
+		(cell1Content === cell2Content &&
+			cell2Content === cell3Content &&
+			cell1Content === "X") ||
+		(cell4Content === cell5Content &&
+			cell5Content === cell6Content &&
+			cell4Content === "X") ||
+		(cell7Content === cell8Content &&
+			cell8Content === cell9Content &&
+			cell7Content === "X") ||
+		// Columns
+		(cell1Content === cell4Content &&
+			cell4Content === cell7Content &&
+			cell1Content === "X") ||
+		(cell2Content === cell5Content &&
+			cell5Content === cell8Content &&
+			cell2Content === "X") ||
+		(cell3Content === cell6Content &&
+			cell6Content === cell9Content &&
+			cell3Content === "X") ||
+		// Diagonals
+		(cell1Content === cell5Content &&
+			cell5Content === cell9Content &&
+			cell1Content === "X") ||
+		(cell3Content === cell5Content &&
+			cell5Content === cell7Content &&
+			cell3Content === "X")
+	) {
+		xWins++;
+		gameOver();
+	} else if (
+		// Rows
+		(cell1Content === cell2Content &&
+			cell2Content === cell3Content &&
+			cell1Content === "O") ||
+		(cell4Content === cell5Content &&
+			cell5Content === cell6Content &&
+			cell4Content === "O") ||
+		(cell7Content === cell8Content &&
+			cell8Content === cell9Content &&
+			cell7Content === "O") ||
+		// Columns
+		(cell1Content === cell4Content &&
+			cell4Content === cell7Content &&
+			cell1Content === "O") ||
+		(cell2Content === cell5Content &&
+			cell5Content === cell8Content &&
+			cell2Content === "O") ||
+		(cell3Content === cell6Content &&
+			cell6Content === cell9Content &&
+			cell3Content === "O") ||
+		// Diagonals
+		(cell1Content === cell5Content &&
+			cell5Content === cell9Content &&
+			cell1Content === "O") ||
+		(cell3Content === cell5Content &&
+			cell5Content === cell7Content &&
+			cell3Content === "O")
+	) {
+		oWins++;
+		gameOver();
+	} else if (
+		// Check for a draw
+		cell1Content !== "" &&
+		cell2Content !== "" &&
+		cell3Content !== "" &&
+		cell4Content !== "" &&
+		cell5Content !== "" &&
+		cell6Content !== "" &&
+		cell7Content !== "" &&
+		cell8Content !== "" &&
+		cell9Content !== ""
+	) {
+		gameOver();
+	}
 }
 
 function gameOver() {
-  state = "gameover";
+	state = "gameover";
+	drawGameOver();
 
-  drawGameOver();
-
-  setTimeout(reset, 2000);
+	setTimeout(reset, 2000);
 }
 
 function drawGame() {
-  drawMainComponents();
+	drawMainComponents();
 }
 
 // Draw Game Over Screen
 function drawGameOver() {
-  drawMainComponents();
+	drawMainComponents();
 
-  // Game Over Text
-  ctx.font = "60px Consolas";
-  ctx.fillStyle = "red";
-  ctx.fillText("GAME OVER", 280, 350);
+	// Game Over Text
+	ctx.font = "80px Consolas";
+	ctx.fillStyle = "red";
+	ctx.fillText("GAME OVER", 250, 350);
 }
 
 // HELPER FUNCTIONS
 function reset() {
-  frameCount = 0;
-  state = "start";
+	state = "start";
+	currentPlayer = "X";
+	cell1Content = "";
+	cell2Content = "";
+	cell3Content = "";
+	cell4Content = "";
+	cell5Content = "";
+	cell6Content = "";
+	cell7Content = "";
+	cell8Content = "";
+	cell9Content = "";
 }
 
 function drawMainComponents() {
-  // Background
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, cnv.width, cnv.height);
+	// Background
+	ctx.fillStyle = "black";
+	ctx.fillRect(0, 0, cnv.width, cnv.height);
 
-  // Draw Grid
-  ctx.fillStyle = "white";
-  ctx.fillRect(266, 0, 50, 800);
-  ctx.fillRect(533, 0, 50, 800);
-  ctx.fillRect(0, 200, cnv.width, 50);
-  ctx.fillRect(0, 500, cnv.width, 50);
+	// Draw Grid
+	ctx.fillStyle = "white";
+	ctx.fillRect(266, 0, 50, 800);
+	ctx.fillRect(533, 0, 50, 800);
+	ctx.fillRect(0, 200, cnv.width, 50);
+	ctx.fillRect(0, 500, cnv.width, 50);
 
-  // Draw Text Bar
-  ctx.fillStyle = "blue";
-  ctx.fillRect(0, 800, cnv.width, 900);
+	// Draw Text Bar
+	ctx.fillStyle = "blue";
+	ctx.fillRect(0, 800, cnv.width, 900);
 
-  // Blue Bar Text
-  ctx.font = "30px Consolas";
-  ctx.fillStyle = "black";
-  ctx.fillText("TIC TAC TOE: " + currentPlayer + " is playing", 25, 840);
-  ctx.fillText("Your Wins: 0 ", 25, cnv.height - 15);
-  ctx.fillText("Computer Wins: 0", cnv.width - 300, cnv.height - 15);
+	// Blue Bar Text
+	ctx.font = "30px Consolas";
+	ctx.fillStyle = "black";
+	ctx.fillText("TIC TAC TOE: Player " + currentPlayer + " is playing", 25, 840);
+	ctx.fillText("X wins : " + xWins, 25, cnv.height - 15);
+	ctx.fillText("O Wins: " + oWins, 600, cnv.height - 15);
 
-  // Draw X's and O's
-  if (cell1Content === "X") {
-    ctx.drawImage(crossImg, 90, 50, 100, 100);
-  } else if (cell1Content === "O") {
-    ctx.drawImage(circleImg, 90, 50, 100, 100);
-  }
+	// Draw X's and O's
+	if (cell1Content === "X") {
+		ctx.drawImage(crossImg, 90, 50, 100, 100);
+	} else if (cell1Content === "O") {
+		ctx.drawImage(circleImg, 90, 50, 100, 100);
+	}
 
-  if (cell2Content === "X") {
-    ctx.drawImage(crossImg, 375, 50, 100, 100);
-  } else if (cell2Content === "O") {
-    ctx.drawImage(circleImg, 90, 50, 100, 100);
-  }
+	if (cell2Content === "X") {
+		ctx.drawImage(crossImg, 375, 50, 100, 100);
+	} else if (cell2Content === "O") {
+		ctx.drawImage(circleImg, 375, 50, 100, 100);
+	}
+
+	if (cell3Content === "X") {
+		ctx.drawImage(crossImg, 650, 50, 100, 100);
+	} else if (cell3Content === "O") {
+		ctx.drawImage(circleImg, 650, 50, 100, 100);
+	}
+
+	if (cell4Content === "X") {
+		ctx.drawImage(crossImg, 90, 330, 100, 100);
+	} else if (cell4Content === "O") {
+		ctx.drawImage(circleImg, 90, 330, 100, 100);
+	}
+
+	if (cell5Content === "X") {
+		ctx.drawImage(crossImg, 375, 330, 100, 100);
+	} else if (cell5Content === "O") {
+		ctx.drawImage(circleImg, 375, 330, 100, 100);
+	}
+
+	if (cell6Content === "X") {
+		ctx.drawImage(crossImg, 640, 330, 100, 100);
+	} else if (cell6Content === "O") {
+		ctx.drawImage(circleImg, 640, 330, 100, 100);
+	}
+
+	if (cell7Content === "X") {
+		ctx.drawImage(crossImg, 90, 630, 100, 100);
+	} else if (cell7Content === "O") {
+		ctx.drawImage(circleImg, 90, 630, 100, 100);
+	}
+
+	if (cell8Content === "X") {
+		ctx.drawImage(crossImg, 375, 630, 100, 100);
+	} else if (cell8Content === "O") {
+		ctx.drawImage(circleImg, 375, 630, 100, 100);
+	}
+
+	if (cell9Content === "X") {
+		ctx.drawImage(crossImg, 640, 630, 100, 100);
+	} else if (cell9Content === "O") {
+		ctx.drawImage(circleImg, 640, 630, 100, 100);
+	}
 }
-
-// x and y positions of images down below (use cross.x and cross.y)
-
-// if (column === 1 && row === 1) {
-//   cross.x = 90;
-//   cross.y = 50;
-// } else if (column === 2 && row === 1) {
-//   cross.x = 375;
-//   cross.y = 50;
-// } else if (column === 3 && row === 1) {
-//   cross.x = 640;
-//   cross.y = 50;
-// } else if (column === 1 && row === 2) {
-//   cross.x = 90;
-//   cross.y = 330;
-// } else if (column === 2 && row === 2) {
-//   cross.x = 375;
-//   cross.y = 330;
-// } else if (column === 3 && row === 2) {
-//   cross.x = 640;
-//   cross.y = 330;
-// } else if (column === 1 && row === 3) {
-//   cross.x = 90;
-//   cross.y = 630;
-// } else if (column === 2 && row === 3) {
-//   cross.x = 375;
-//   cross.y = 630;
-// } else if (column === 3 && row === 3) {
-//   cross.x = 640;
-//   cross.y = 630;
-// }
